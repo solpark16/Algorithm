50c90ed17c6d7bde3df22a1d48cad82de91d884a
Sol Park <79960346+solpark16@users.noreply.github.com>
2024-03-25 18:15:01 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,11 +1,3 @@
 function solution(my_string) {
-    var vowers = ['a','e','i','o','u'];
-    return [...my_string].filter((item)=>{
-        for (let i=0;i<vowers.length;i++){
-            if (item.includes(vowers[i])){
-                return false;
-            }
-        }
-        return true;
-    }).join("");
+    return my_string.replace(/[aeiou]/g, '');
 }
