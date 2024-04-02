a17a7873a6cb82ed335bacc1850762d1f5654d17
Sol Park <79960346+solpark16@users.noreply.github.com>
2024-04-02 14:37:18 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,15 +1,3 @@
 function solution(age) {
-    var answer = '';
-    let alpha = [];
-    let arr = Array.from(String(age));
-    
-    for (let i=97; i<=122; i++){
-        alpha.push(String.fromCharCode(i));
-    }
-    
-    for (let j=0; j<arr.length; j++){
-        answer += alpha[arr[j]];
-    }
-    
-    return answer;
+    return age.toString().split("").map((v)=>"abcdefghij"[v]).join('');
 }
