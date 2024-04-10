33e4429d0a650f5d64d6a07b72c1f33a2e44371b
Sol Park <79960346+solpark16@users.noreply.github.com>
2024-04-10 18:20:19 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,8 +1,4 @@
 function solution(emergency) {
     let arr = [...emergency].sort((a,b)=>b-a);
-    let result = [];
-    for(let i=0; i<arr.length; i++){
-        result.push(arr.indexOf(emergency[i])+1);
-    }
-    return result;
+    return emergency.map(v=>arr.indexOf(v)+1);
 }
