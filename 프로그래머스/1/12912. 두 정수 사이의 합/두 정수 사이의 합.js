c31e77d573f7a259434e34d7dd07c9bf5ef831a6
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-04-27 17:19:58 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    let result = 0;
+    if (a<b){
+        for (let i = a; i <= b; i++){
+            result += i;
+        }
+    } else {
+        for (let i = b; i <= a; i++){
+            result += i;
+        }
+    }
+
+    return result
+}
