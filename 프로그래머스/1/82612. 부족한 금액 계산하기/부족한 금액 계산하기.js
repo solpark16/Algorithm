a61f803d4567a2a3e2712aa5b4e5864a3301ee5c
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-04-30 08:07:35 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,8 @@
+function solution(price, money, count) {
+    let sum = 0;
+    for (let i=1; i<=count; i++){
+        sum += price * i;
+    }
+    if (sum>money) return sum - money;
+    else return 0;
+}
