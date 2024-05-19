e95a58699477ff8a32b64344d4f7926851df66c2
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-05-19 18:16:25 +0900

    [level 1] Title: 대충 만든 자판, Time: 13.11 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,23 @@
+function solution(keymap, targets) {
+  let result = targets.map((target) => {
+    let targetArr = [...target];
+    let countSum = 0;
+    for (let i = 0; i < targetArr.length; i++) {
+      let count = 0;
+      for (let j = 0; j < keymap.length; j++) {
+        if (keymap[j].includes(target[i])) {
+            if (count === 0 || count > keymap[j].indexOf(target[i]) + 1) {
+                count = keymap[j].indexOf(target[i]) + 1;
+            }
+        }
+      }
+      if (count === 0) {
+        return -1;
+      } else {
+        countSum += count;
+      }
+    }
+    return countSum;
+  });
+  return result;
+}
