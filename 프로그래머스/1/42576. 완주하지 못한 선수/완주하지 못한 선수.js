95e962526d94e05b02686e30acefa292719a756e
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-09-01 19:12:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 124.10 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,9 @@
 function solution(particpant, completion) {
-  const sortedParticipant = particpant.sort();
-  const sortedCompletion = completion.sort();
-  for (let i = 0; i < sortedParticipant.length; i++) {
-    if (sortedParticipant[i] === sortedCompletion[i]) {
-      continue;
+  particpant.sort();
+  completion.sort();
+  for (let i = 0; i < particpant.length; i++) {
+    if (particpant[i] !== completion[i]) {
+      return particpant[i];
     }
-    return sortedParticipant[i];
   }
 }
