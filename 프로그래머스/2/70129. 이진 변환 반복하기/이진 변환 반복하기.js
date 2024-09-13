457c1309ddbee38e1711609865a2b71d04330d0f
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-09-13 14:43:16 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.49 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,10 +1,10 @@
 function solution(s) {
   const result = [0, 0];
   while (s !== "1") {
-    const zero = s.split("").filter((el) => el !== "0");
+    const filteredS = s.split("").filter((el) => el !== "0");
     result[0] += 1;
-    result[1] += s.length - zero.length;
-    s = zero.join("").length.toString(2);
+    result[1] += s.length - filteredS.length;
+    s = filteredS.join("").length.toString(2);
   }
   return result;
 }
