76519ba6d0e3455f5035a83a7598643a2f227e3f
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-09-19 12:43:25 +0900

    [level 2] Title: 구명보트, Time: 38.80 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,11 @@
+function solution(people, limit) {
+  people.sort((a, b) => b - a);
+  let num = 0;
+  for (let i = 0; i < people.length; i++) {
+    num++;
+    if (people[i] + people[people.length - 1] <= limit) {
+      people.splice(people.length - 1, 1);
+    }
+  }
+  return num;
+}
