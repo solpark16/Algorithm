75407a6f2f7edbcc69294a1b50ba9288ea158bb5
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-10-13 17:50:47 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,11 +1,7 @@
 function solution(num_list) {
   const arr = [0, 0];
   for (let i = 0; i < num_list.length; i++) {
-    if (i % 2 === 0) {
-      arr[0] += num_list[i];
-    } else {
-      arr[1] += num_list[i];
-    }
+    i % 2 === 0 ? (arr[0] += num_list[i]) : (arr[1] += num_list[i]);
   }
   return Math.max(...arr);
 }
