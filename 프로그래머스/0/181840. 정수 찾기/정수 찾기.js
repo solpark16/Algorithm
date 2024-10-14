03bef0910f261078da29f2c0e6aca55d87d42c0c
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-10-14 10:24:28 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -1,8 +1,3 @@
 function solution(num_list, n) {
-  for (let i = 0; i < num_list.length; i++) {
-    if (num_list[i] === n) {
-      return 1;
-    }
-  }
-  return 0;
+  return num_list.includes(n) ? 1 : 0;
 }
