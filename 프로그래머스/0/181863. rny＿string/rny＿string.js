54493d5baa7b6f35d71ba83902001cdd9a99b27f
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-10-27 14:31:07 +0900

    [level 0] Title: rny_string, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -0,0 +1,11 @@
+function solution(rny_string) {
+  let result = "";
+  for (let i = 0; i < rny_string.length; i++) {
+    if (rny_string[i] === "m") {
+      result += "rn";
+    } else {
+      result += rny_string[i];
+    }
+  }
+  return result;
+}
