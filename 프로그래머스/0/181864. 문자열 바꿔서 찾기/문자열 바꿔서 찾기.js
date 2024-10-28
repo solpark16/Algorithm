5632bc6640b7d2162aa4996e3c8901a8d023eeca
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-10-28 10:15:57 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,14 +1,14 @@
 function solution(myString, pat) {
-  return Number(
-    [...myString]
-      .map((el) => {
-        if (el === "A") {
-          return "B";
-        } else {
-          return "A";
-        }
-      })
-      .join("")
-      .includes(pat)
-  );
+  return [...myString]
+    .map((el) => {
+      if (el === "A") {
+        return "B";
+      } else {
+        return "A";
+      }
+    })
+    .join("")
+    .includes(pat)
+    ? 1
+    : 0;
 }
