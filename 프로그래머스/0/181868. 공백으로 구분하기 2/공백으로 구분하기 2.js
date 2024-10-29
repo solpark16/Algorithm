30d95aa40dcf4e19e3b623a16837ff89428cac0f
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-10-29 09:51:37 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,9 +1,3 @@
 function solution(my_string) {
-  const result = [];
-  my_string.split(" ").forEach((el) => {
-    if (el) {
-      result.push(el);
-    }
-  });
-  return result;
+  return my_string.split(" ").filter((el) => el)
 }
