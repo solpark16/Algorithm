a3abaf9fe8ec593b60c33d2bdea37cda2254a763
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-11-06 10:32:00 +0900

    [level 0] Title: 할 일 목록, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,9 +1,3 @@
 function solution(todo_list, finished) {
-  const result = [];
-  for (let i = 0; i < todo_list.length; i++) {
-    if (!finished[i]) {
-      result.push(todo_list[i]);
-    }
-  }
-  return result;
+    return todo_list.filter((el, idx) => !finished[idx]);
 }
