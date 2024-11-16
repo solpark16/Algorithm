9322abf7e3ddb76381f92b2c8d8640c27ce17f2c
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-11-16 20:15:54 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+  let odd = "";
+  let even = "";
+  for (let i = 0; i < num_list.length; i++) {
+    if (num_list[i] % 2 === 0) {
+      even += num_list[i];
+    } else {
+      odd += num_list[i];
+    }
+  }
+  return +odd + +even;
+}
