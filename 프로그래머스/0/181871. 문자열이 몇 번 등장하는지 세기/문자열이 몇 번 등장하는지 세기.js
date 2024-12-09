a96442cbbda9b3871bd67b6df2c049e225597323
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-12-09 16:38:33 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,18 +1,7 @@
 function solution(myString, pat) {
-    function patCheck(i){
-        for (let j=0; j<pat.length; j++){
-            if (myString[i+j] !== pat[j]){
-                return false;
-            }
-        }
-        return true;
-    }
     let result = 0;
-    for (let i=0; i<myString.length - pat.length + 1; i++){
-        if (myString[i]!==pat[0]) {
-            continue;
-        }
-        if (patCheck(i)){
+    for (let i=0; i <= myString.length - pat.length; i++){
+        if (myString.slice(i,i+pat.length)===pat){
             result++;
         }
     }
