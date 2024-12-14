91b07541115416f6410a65788fd1f1f294e85a14
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-12-14 16:34:56 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,11 @@
+function solution(ineq, eq, n, m) {
+    if (ineq===">"&&eq==="="){
+        return Number(n >= m)
+    } else if (ineq==="<"&&eq==="="){
+        return Number(n <= m)
+    } else if (ineq===">"&&eq==="!"){
+        return Number(n > m)
+    } else {
+        return Number(n < m)
+    }
+}
