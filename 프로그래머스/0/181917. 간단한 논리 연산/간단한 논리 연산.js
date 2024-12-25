6d12308048ff4550dc4a9a8b5813d87422e1b41b
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-12-25 13:38:26 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -1,11 +1,3 @@
 function solution(x1, x2, x3, x4) {
-    let a = false;
-    let b = false;
-    if(x1||x2){
-        a = true;
-    }
-    if(x3||x4){
-        b = true;
-    }
-    return a && b ? true : false
+    return (x1||x2) && (x3||x4) ? true : false
 }
