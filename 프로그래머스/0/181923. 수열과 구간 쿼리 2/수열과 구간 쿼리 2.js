35925c665df855e7ae02361f06f479ad187f79f8
박솔 (Sol Park) <79960346+solpark16@users.noreply.github.com>
2024-12-31 20:26:17 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 25.91 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,9 +1,9 @@
 function solution(arr, queries) {
-    var answer = [];
+    const result = [];
     for(let [s,e,k] of queries){
-        let temp = arr.filter((v,i)=>(i>=s&&i<=e&&v>k)).sort((a,b)=>a-b)[0]
-        answer.push(temp?temp:-1)
+        const el = arr.filter((v,i)=>(i>=s && i<=e && v>k)).sort((a,b)=>a-b)[0]
+        result.push(el ? el : -1)
     }
 
-    return answer;
+    return result;
 }
